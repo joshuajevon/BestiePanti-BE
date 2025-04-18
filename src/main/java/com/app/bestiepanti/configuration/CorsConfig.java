27118702538f8d9a040049356bffa837930a11b6
joshuajevon <joshuajevon05@gmail.com>
2025-04-18 11:01:30 +0700

    fix: allowed headers

--- src/main/java/com/app/bestiepanti/configuration/CorsConfig.java
+++ src/main/java/com/app/bestiepanti/configuration/CorsConfig.java
@@ -18,9 +18,9 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of(urlFrontEnd));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH"));
-        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowedOrigins(List.of("https://bestie-panti-fe.vercel.app"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept"));
         configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
